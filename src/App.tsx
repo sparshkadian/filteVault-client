810f6b3c670f7c6040174e3a33da931ad37423ca
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-09 20:36:42 +0530

    added NavBar with dummy DropDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Hero from './pages/Hero';
+import NavBar from './components/NavBar';
 
 const App = () => {
   return (
-    <div className='h-screen w-screen'>
+    <div className='h-screen w-screen relative'>
+      <NavBar />
       <Hero />
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,56 @@
-import { Dropdown, Space, MenuProps } from 'antd';
+import { MenuProps } from 'antd';
+import { Dropdown, Space } from 'antd';
 
-import { items } from '../constants';
+const items: MenuProps['items'] = [
+  {
+    key: '1',
+    label: (
+      <a target='_blank' href='https://www.antgroup.com'>
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <a target='_blank' href='https://www.aliyun.com'>
+        2nd menu item
+      </a>
+    ),
+  },
+];
+
+const spaceClass = 'hover:bg-gray-100 px-4 py-3 rounded-full';
 
 const NavBar = () => {
   return (
-    <div className='flex gap-5 p-5'>
-      <p>TechTrade</p>
-      <Dropdown menu={{ items }}>
-        <Space className='cursor-pointer'>Laptops</Space>
-      </Dropdown>
-
-      {/* <Dropdown menu={{ items }}>
-        <Space className='cursor-pointer'>Mobiles</Space>
-      </Dropdown> */}
+    <div className='border navbar absolute z-[1] flex justify-center rounded-full bg-white shadow-2xl p-2'>
+      <div className='flex gap-10  font-semibold cursor-pointer'>
+        <Dropdown
+          arrow={true}
+          placement='bottomCenter'
+          className='text-xl'
+          menu={{ items }}
+        >
+          <Space className={`${spaceClass}`}>Laptops</Space>
+        </Dropdown>
+        <Dropdown
+          arrow={true}
+          placement='bottomCenter'
+          className='text-xl'
+          menu={{ items }}
+        >
+          <Space className={`${spaceClass}`}>Desktops</Space>
+        </Dropdown>
+        <Dropdown
+          arrow={true}
+          placement='bottomCenter'
+          className='text-xl'
+          menu={{ items }}
+        >
+          <Space className={`${spaceClass}`}>Mobiles</Space>
+        </Dropdown>
+      </div>
     </div>
   );
 };
