1cda12f3875988259083207f2f8fb97edc42a984
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-17 13:52:37 +0530

    added logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,12 @@ const Navbar = () => {
   const location = useLocation();
 
   return (
-    <div className='absolute top-0 w-screen h-[10vh] z-10 px-4 flex justify-between items-center'>
-      <div>Logo</div>
-      {location.pathname !== ('/login' || '/signup') && (
-        <div className='flex gap-6 font-semibold'>
+    <div className='absolute top-0 w-screen h-[12vh] z-10 px-4 pr-6 flex justify-between items-center'>
+      <Link to='/'>
+        <img src='./vault.png' alt='logo' width={45} />
+      </Link>
+      {location.pathname !== '/signup' && location.pathname !== '/login' && (
+        <div className='flex gap-8 font-semibold'>
           <Link to='/signup'>
             <button className='nav__button'>Sign up</button>
           </Link>
