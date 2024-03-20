c22fa1c09b7d50e6c32f1dcf36d68142f6be6a10
Sparsh Kadian <sparshkadian323@gmail.com>
2024-03-20 16:30:40 +0530

    changed querySearch Bar

--- src/components/SearchFile.tsx
+++ src/components/SearchFile.tsx
@@ -1,26 +1,37 @@
 import { SetStateAction } from 'react';
+import { useState } from 'react';
 
 const SearchFile = ({
   setSearchFile,
 }: {
   setSearchFile: React.Dispatch<SetStateAction<string>>;
 }) => {
+  const [query, setQuery] = useState<string>('');
+
   return (
     <div className='mx-auto mt-3 flex items-center justify-between max-w-2xl bg-gray-100 px-3 py-2 rounded-full shadow-md hover:shadow-xl focus:shadow-xl'>
       <input
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           setSearchFile(e.target.value);
+          setQuery(e.target.value);
         }}
         type='text'
+        value={query}
         className='w-[90%] p-1 bg-gray-100 focus:outline-none'
         placeholder='Search File'
       />
-      <img
-        src='./search.png'
-        alt='search'
-        width={20}
-        className='cursor-pointer'
-      />
+      {query.length > 0 && (
+        <img
+          onClick={() => {
+            setSearchFile('');
+            setQuery('');
+          }}
+          src='./close.png'
+          alt='search'
+          width={15}
+          className='cursor-pointer mr-1'
+        />
+      )}
     </div>
   );
 };
